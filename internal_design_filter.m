8f04da223fe9491c1e842647ddefe2677fc97a88
Di Pu <di.pu@analog.com>
2016-11-18 09:43:10 -0800

    editted the comments before functions

--- internal_design_filter.m
+++ internal_design_filter.m
@@ -31,7 +31,7 @@
 %  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 %  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 %  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+%
 % Inputs (structure containing the following fields)
 % ============================================
 % Rdata      = input/output sample data rate (in Hz)
