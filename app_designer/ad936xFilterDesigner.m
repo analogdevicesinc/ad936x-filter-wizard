e3dbac505d0af89ab952ebae535b4aa33e2b8a46
Travis Collins <travis.collins@analog.com>
2017-10-09 21:23:41 -0400

    Added fields and checking to designer management class for filter designing.
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- app_designer/ad936xFilterDesigner.m
+++ app_designer/ad936xFilterDesigner.m
@@ -3,94 +3,131 @@ classdef ad936xFilterDesigner < handle
     %
     
     properties
-%        TX = struct('HB1',1,'HB2',1);
-%        RX
-        HB1 = 1;
-        HB2 = 1;
-        HB3 = 1;
-        FIR = 1;
-        FPGA_Filter = false;
-        RData = 30.72e6;
+        % Chip specific configurations
+        HB1 = 2;
+        HB2 = 2;
+        HB3 = 2;
+        FIR = 2;
+        FPGA_Filter = false; % Pluto FPGA filter enabled
+        Rdata = 7680000; % Data Rate at chip output
+        PLL_mult = 8;
+        converter_rate = 122880000;
+        PLL_rate = 983040000;
+        caldiv = 25;
+        RFbw = 6196961;
+        FIRdBmin = 0;
+        % Design criteria
+        Fpass = 2560000;
+        Fstop = 3200000;
+        Fcenter = 0;
+        Apass = 0.5000;
+        Astop = 80;
+        phEQ = -1;
+        wnom = 4480000;
+        Type = 'Lowpass';
+        RxTx = 'Rx';
+        % Results from designer
+        GroupDelay = -1;
+        GroupDelayVariance = -1;
+        ApassGain = -1;
+        AstopGain = -1;
+        MaxInput = -1;
+        NumTaps = -1;
+        Taps = [];
+        
+    end
+    
+    properties (Constant, Access = private)
+        AD9361_MAX_RATE = 61440000;
+        AD9361_MIN_RATE = 520833;
+        AD9361_MAX_RX_HB1 = 245760000;
+        AD9361_MAX_RX_HB2 = 320000000;
+        AD9361_MAX_RX_HB3 = 640000000;
+        AD9361_MAX_TX_HB1 = 160000000;
+        AD9361_MAX_TX_HB2 = 320000000;
+        AD9361_MAX_TX_HB3 = 320000000;
+        AD9361_MAX_FIR = 61440000 * 2
+        AD9361_MAX_BBPLL_FREQ = 1430000000;
+        AD9361_MIN_BBPLL_FREQ = 715000000;
+        AD9361_MIN_ADC_CLK = 25000000;
+        AD9361_MAX_ADC_CLK = 640000000;
+        AD9361_MIN_DAC_CLK = 25000000;
+        AD9361_MAX_DAC_CLK = 640000000 / 2
     end
     
     
-    
     methods
-%         % Constructor
-%         function obj = ad936xFilterDesigner(varargin)
-%             setProperties(obj,nargin,varargin{:});
-%         end
+        % Constructor
+        function obj = ad936xFilterDesigner(varargin)
+%                     setProperties(obj,nargin,varargin{:});
+        end
         %% Check properties
         
-        % Data rate
-        function set.RData(obj, value)
+        function set.Rdata(obj, value)
             validateattributes( value, { 'double','single' }, ...
-                {'>', 0.1, '<=', 61.44,'real', 'positive', 'scalar', 'finite',...
+                {'>=', 0.1e6, '<=', 61.44e6,'real', 'positive', 'scalar', 'finite',...
                 'nonnan', 'nonempty'}, ...
                 '', 'RData');
-            % Scale to MHZ
-            obj.RData = value*1e6;
+            obj.Rdata = value;
+        end
+        function set.HB1(obj, value)
+            validateattributes( value, { 'double','single' }, ...
+                {'>=', 1, '<=', 2,'real', 'positive', 'scalar', 'finite',...
+                'nonnan', 'nonempty', 'integer'}, ...
+                '','HB1');
+            obj.HB1 = double(value);
+        end
+        function set.HB2(obj, value)
+            validateattributes( value, { 'double','single' }, ...
+                {'>=', 1, '<=', 2,'real', 'positive', 'scalar', 'finite',...
+                'nonnan', 'nonempty', 'integer'}, ...
+                '','HB2');
+            obj.HB2 = double(value);
+        end
+        function set.HB3(obj, value)
+            validateattributes( value, { 'double','single' }, ...
+                {'>=', 1, '<=', 3,'real', 'positive', 'scalar', 'finite',...
+                'nonnan', 'nonempty', 'integer'}, ...
+                '','HB3');
+            obj.HB3 = double(value);
+        end
+        function set.FIR(obj, value)
+            validateattributes( value, { 'double','single' }, ...
+                {'real', 'positive', 'scalar', 'finite',...
+                'nonnan', 'nonempty', 'integer'}, ...
+                '','FIR');
+            vals = 2.^(0:2);
+            if ~sum(vals==value)
+                error(['Expected FIR to be any of: ',num2str(vals)]);
+            end
+            isa(value,vals);
+            obj.FIR = double(value);
+        end
+        function set.PLL_mult(obj, value)
+            validateattributes( value, { 'double','single' }, ...
+                {'real', 'positive', 'scalar', 'finite',...
+                'nonnan', 'nonempty', 'integer'}, ...
+                '','PLL_mult');
+            vals = 2.^(0:6);
+            if ~sum(vals==value)
+                error(['Expected PLL_mult to be any of: ',num2str(vals)]);
+            end
+            isa(value,vals);
+            obj.PLL_mult = double(value);
         end
         
-%         %% Utility function
-%         function autoselect_rates(obj)
-%             % sanity check the PLL rate and DAC divider values and alter them if necessary
-%             if isfield(handles, 'tx') && isfield(handles, 'rx')
-%                 if (obj.rx.PLL_rate ~= obj.tx.PLL_rate)
-%                     % If dec3 is used by Rx or Tx, both must use it in order for the
-%                     % PLL rates to match.
-%                     if obj.tx.HB3 == 3 || obj.rx.HB3 == 3
-%                         obj.rx.HB3 = 3;
-%                         obj.tx.HB3 = 3;
-%                     end
-%                     
-%                     ADC_rate = obj.rx.Rdata * obj.rx.FIR * ...
-%                         obj.rx.HB1 * obj.rx.HB2 * obj.rx.HB3;
-%                     DAC_rate = obj.tx.Rdata * obj.tx.FIR * ...
-%                         obj.tx.HB1 * obj.tx.HB2 * obj.tx.HB3;
-%                     DAC_div = ADC_rate / DAC_rate;
-%                     if (obj.tx.DAC_div ~= DAC_div)
-%                         if (DAC_div == 1 || DAC_div == 2)
-%                             obj.tx.DAC_div = DAC_div;
-%                             obj.tx.PLL_mult = obj.rx.PLL_mult;
-%                             filter_type = get(handles.filter_type, 'Value');
-%                             set(handles.filter_type, 'Value', 0);
-%                             obj.tx.caldiv = default_caldiv(handles);
-%                             set(handles.filter_type, 'Value', filter_type);
-%                         end
-%                     end
-%                     
-%                     obj.rx.PLL_mult = fastest_FIR([64 32 16 8 4 2 1], handles.bounds.MAX_BBPLL_FREQ, handles.bounds.MIN_BBPLL_FREQ, ...
-%                         obj.rx.Rdata * obj.rx.FIR * obj.rx.HB1 * obj.rx.HB2 * obj.rx.HB3 * obj.rx.DAC_div);
-%                     obj.tx.PLL_mult = obj.rx.PLL_mult;
-%                     
-%                     if obj.rx.PLL_mult > 64
-%                         X = ['Date rate = ', num2str(tohwTx.TXSAMP), ' Hz. Tx BBPLL is too high for Rx to match.'];
-%                         disp(X);
-%                     end
-%                     
-%                     obj.rx.PLL_rate = obj.rx.Rdata * obj.rx.FIR * obj.rx.HB1 * ...
-%                         obj.rx.HB2 * obj.rx.HB3 * obj.rx.PLL_mult;
-%                 else
-%                     ADC_rate = obj.rx.Rdata * obj.rx.FIR * ...
-%                         obj.rx.HB1 * obj.rx.HB2 * obj.rx.HB3;
-%                     DAC_rate = obj.tx.Rdata * obj.tx.FIR * ...
-%                         obj.tx.HB1 * obj.tx.HB2 * obj.tx.HB3;
-%                     DAC_div = ADC_rate / DAC_rate;
-%                     if (obj.tx.DAC_div ~= DAC_div)
-%                         if (DAC_div == 1 || DAC_div == 2)
-%                             obj.tx.DAC_div = DAC_div;
-%                             obj.tx.PLL_mult = obj.rx.PLL_mult;
-%                             filter_type = get(handles.filter_type, 'Value');
-%                             set(handles.filter_type, 'Value', 0);
-%                             obj.tx.caldiv = default_caldiv(handles);
-%                             set(handles.filter_type, 'Value', filter_type);
-%                         end
-%                     end
-%                 end
-%             end
-%             
-%         end
+    end
+    
+    methods (Access = private)
         
+        %% Check path rates
+        function ValidatePathRates(obj)
+            
+        end
+        
+        %% Call designer
+        function DesignFilter(obj)
+        
+        end
     end
 end
