dc1f1b9677a2535dc99baed8336f4d68bcea12d6
Travis Collins <travis.collins@analog.com>
2019-04-19 15:40:31 -0400

    Add specific tests for LTE filters
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- test/FilterWizardTests.m
+++ test/FilterWizardTests.m
@@ -2,6 +2,8 @@ classdef FilterWizardTests < matlab.unittest.TestCase
     
     properties
         SampleRates = [0.52083333,0.6:0.1:61.44].*1e6;
+        LTEModes = {'LTE5','LTE10','LTE15','LTE20'};
+        MaxRippleDB = 1;
     end
     
     methods(TestClassSetup)
@@ -15,11 +17,24 @@ classdef FilterWizardTests < matlab.unittest.TestCase
         function testAutoGenerationRipple(testCase)
             import matlab.unittest.constraints.IsLessThanOrEqualTo
             sr = testCase.SampleRates;
+            limit = testCase.MaxRippleDB;
             % Test ripple of generated filters           
             parfor r = 1:length(sr)
                 out = internal_design_filter_opt_ripple(sr(r));
-                verifyThat(testCase, out.Apass_actual, IsLessThanOrEqualTo(out.Apass), ...
-                    sprintf('Generated filter for rate %d failed to meet ripple target',sr(r)))
+                verifyThat(testCase, out.Apass_actual, IsLessThanOrEqualTo(limit), ...
+                    sprintf('Generated filter for rate %d with ripple %f (Limit %f)',...
+                    sr(r),out.Apass_actual,limit))
+            end
+        end
+        
+        function testLTEFilterGeneration(testCase)
+            import matlab.unittest.constraints.IsEqualTo
+            % Verify LTE filters are created correctly
+            for m = testCase.LTEModes
+                i = load([m{:},'_input.mat']);
+                o = load([m{:},'_output.mat']);
+                output = internal_design_filter(i.input);
+                testCase.verifyThat(output,IsEqualTo(o.output));
             end
         end
         
